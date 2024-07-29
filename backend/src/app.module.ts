159b38e53afbe5442fd9b48c77b0590d399dfc0b
NemethTamas1 <nemethtamas029@gmail.com>
2024-07-29 10:10:26 +0200

    User interface and form submission first steps

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(), 
-    BooksModule
+    BooksModule,
+    UsersModule
   ],
 })
 export class AppModule {}
