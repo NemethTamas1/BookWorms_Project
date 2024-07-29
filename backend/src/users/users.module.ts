2c37c357c250b1e7914c80f91f721f88b3173020
Fantazia01 <tothfabian01@gmail.com>
2024-07-29 13:35:01 +0200

    fixing bugs

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { DatabaseService } from 'src/database/db.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, DatabaseService],
 })
 export class UsersModule {}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.interface';
 import { DatabaseService } from 'src/database/db.service';
-import { promises } from 'dns';
 
 @Injectable()
 export class UsersService {
     constructor(private readonly dbService: DatabaseService) {}
 
-    async createUser(user: User): Promise<User> {
-        const createdUser = await this.dbService.createUser(user); //?
-        return createdUser;
+    async createUser(user: User) {
+        await this.dbService.turso.execute("INSERT INTO User (first_name, family_name, email, password) VALUES (${user.id}, ${user.first_name}, ${user.family_name}, ${user.email}, ${user.password})");
+        return user;
     }
 }
