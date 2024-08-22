679e1dabf92cc32e63d3a4740f1904012a52f404
Fantazia01 <tothfabian01@gmail.com>
2024-08-22 15:32:20 +0200

    fix the links

--- frontend/e2e/vue.spec.ts
+++ frontend/e2e/vue.spec.ts
@@ -6,13 +6,13 @@ import { test, expect } from '@playwright/test';
 const localURL = import.meta.env.VITE_URL;
 
 test('Checking the title and the heading', async ({ page }) => {
-  await page.goto(localURL+'/books');
+  await page.goto(localURL+'books');
   await expect(page).toHaveTitle(/Könyvek/);
   await expect(page.getByRole('heading', { name: 'BookWorms' , exact:true})).toBeVisible();
 })
 
 test('Checking the name of the books', async ({ page }) => {
-  await page.goto(localURL+'/books');
+  await page.goto(localURL+'books');
   await expect(page.getByRole('heading', { name: 'Angol-magyar szótár' , exact:true})).toBeVisible({ timeout: 10000 });
   await expect(page.getByRole('heading', { name: 'Fotókönyv' , exact:true})).toBeVisible({ timeout: 10000 });
   await expect(page.getByRole('heading', { name: 'A filozófia nagykönyve - Minden, amit tudni érdemes' , exact:true})).toBeVisible({ timeout: 10000 });
