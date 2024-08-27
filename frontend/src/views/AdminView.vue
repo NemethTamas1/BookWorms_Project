2ef34da8a72251f29a6371f342740270a3759a84
Fantazia01 <tothfabian01@gmail.com>
2024-08-27 12:09:33 +0200

    fix paginator with emit

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -7,7 +7,7 @@ import type { Application } from '@/models/Application';
 import type { Book } from '@/models/Book';
 
 // Define the tabs
-const tabs = ['Összes', 'Elfogadásra vár', 'Elfogadott', 'Elutasított'];
+const tabs = ['Összes', 'Nem visszaigazolt', 'Elfogadásra vár', 'Elfogadott', 'Elutasított'];
 
 //const selectedTab = ref(tabs[0]);
 const selectedBookId = ref<number | null>(null); // Use null for "All" option
@@ -17,15 +17,15 @@ const itemsPerPage = 10; // Number of items per page
 const currentPage = ref(0);
 const totalPages = ref(0);
 
-// function updateSelectedTab(tab: string) {
-//   selectedTab.value = tab;
-// }
-
 const applicationsResponse = await useGetApplications();
 const applications = ref<Application[]>(applicationsResponse)
 
 const booksResponse = await useGetBooks();
 const books = ref<Book[]>(booksResponse)
+
+function handleTabChange() {
+  currentPage.value = 0;
+}
 
 // Define filter functions
 function filterApplications(tab: string): Application[] {
@@ -34,6 +34,9 @@ function filterApplications(tab: string): Application[] {
   switch (tab) {
     case 'Összes':
       break; // No filtering
+    case 'Nem visszaigazolt':
+      filteredApplications = filteredApplications.filter(app => app.application_status === 1);
+      break;
     case 'Elfogadásra vár':
       filteredApplications = filteredApplications.filter(app => app.application_status === 2);
       break;
@@ -60,12 +63,12 @@ function paginatedApplications(tab: string): Application[] {
   const filteredApps = filterApplications(tab);
   const start = currentPage.value * itemsPerPage;
   const end = start + itemsPerPage;
-  updateTotalPages(tab);
+  updateTotalPages(tab, filteredApps);
   return filteredApps.slice(start, end);
 }
 
-function updateTotalPages(tab: string) {
-  totalPages.value = Math.ceil(filterApplications(tab).length / itemsPerPage);
+function updateTotalPages(tab: string, filteredApps: Application[]) {
+  totalPages.value = Math.ceil(filteredApps.length / itemsPerPage);
 }
 
 // Change page
@@ -89,8 +92,8 @@ function changePage(newPage: number) {
         </option>
       </select>
     </div>
-    <!-- @update:selectedTab="updateSelectedTab" -->
-    <Tabs :tabs="tabs">
+    <!-- Listen for the 'tab-changed' event and reset currentPage -->
+    <Tabs :tabs="tabs" @tab-changed="handleTabChange">
        <template #default="{ selectedTab }">
         <!-- Pagination Controls -->
         <div class="pagination">
@@ -107,6 +110,20 @@ function changePage(newPage: number) {
           :showUserId="true"
           :showApplicationStatus="true"
           :showPrice="true"
+          :showMotivationalLetter="true"
+          :showCheckImage="false"
+          :showXImage="false"
+          :showBackImage="false"
+        />
+        <!--Applications awaiting e-mail confirmation from user  -->
+        <Table
+          v-if="selectedTab === 'Nem visszaigazolt'"
+          title='Nem visszaigazolt jelentkezések'
+          :applications="paginatedApplications(selectedTab)"
+          :showBookId="true"
+          :showUserId="true"
+          :showApplicationStatus="false"
+          :showPrice="false"
           :showMotivationalLetter="true"
           :showCheckImage="false"
           :showXImage="false"
